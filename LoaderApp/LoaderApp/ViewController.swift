3926a16471533cec4f95522e4891057f12578a31
RomanKorsak <korsak-mc12@list.ru>
2022-04-04 19:46:34 +0300

    Create App
    Create App

---
+++ LoaderApp/LoaderApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoaderApp
+//
+//  Created by Роман Корсак on 4.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
