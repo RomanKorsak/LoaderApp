d8aa09266b1724fca5cd42264fb8da462cb30915
RomanKorsak <korsak-mc12@list.ru>
2022-04-04 21:27:28 +0300

    add UIPickerView

--- LoaderApp/LoaderApp/ViewController.swift
+++ LoaderApp/LoaderApp/ViewController.swift
@@ -5,15 +5,47 @@
 //  Created by Роман Корсак on 4.04.22.
 //
 
+import NVActivityIndicatorView
+import NVActivityIndicatorViewExtended
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, NVActivityIndicatorViewable {
+    
+    let typesOfLoader: [String] = ["ballPulse", "ballSpinFadeLoader", "pacman", "circleStrokeSpin", "ballTrianglePath"]
+    
+    @IBOutlet var picker: UIPickerView!
+    
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        picker.delegate = self
     }
 
-
+    @IBAction func buttonShowAnimation() {
+        startAnimating(type: .ballBeat)
+        
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3) { [weak self] in
+            self?.stopAnimating()
+        }
+    }
 }
 
+extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        typesOfLoader.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return typesOfLoader[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        myLabel.text = typesOfLoader[row]
+    }
+}
