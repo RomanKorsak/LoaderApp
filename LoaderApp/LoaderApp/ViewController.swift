7dcae531c952e144e05107962b8ffcf3c0d53fcc
RomanKorsak <korsak-mc12@list.ru>
2022-04-05 14:24:57 +0300

    add new features
    add new functions for selecting color of loader and text

--- LoaderApp/LoaderApp/ViewController.swift
+++ LoaderApp/LoaderApp/ViewController.swift
@@ -11,20 +11,55 @@ import UIKit
 
 class ViewController: UIViewController, NVActivityIndicatorViewable {
     
-    let typesOfLoader: [String] = ["ballPulse", "ballSpinFadeLoader", "pacman", "circleStrokeSpin", "ballTrianglePath"]
+    let typesOfLoader: [String] = ["ballPulse", "ballSpinFadeLoader", "pacman", "circleStrokeSpin", "ballTrianglePath", "squareSpin"]
+    
+    
+    
     
     @IBOutlet var picker: UIPickerView!
     
+    @IBOutlet weak var textView: UITextView!
     
-    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var colorsSegmentControl: UISegmentedControl!
+    
+    var caseOfLoader: NVActivityIndicatorType = .ballDoubleBounce
+    var textFromTextView: String = ""
+    var colorOfLoader: UIColor = .red
     
     override func viewDidLoad() {
         super.viewDidLoad()
         picker.delegate = self
+        colorsSegmentControl.addTarget(self, action: #selector(self.changeColorOfLoader), for: .valueChanged)
+        colorsSegmentControl.selectedSegmentTintColor = .red
+    }
+    
+    
+    @objc func changeColorOfLoader(){
+        switch colorsSegmentControl.selectedSegmentIndex{
+        case 0:
+            colorOfLoader = .red
+        case 1:
+            colorOfLoader = .blue
+        case 2:
+            colorOfLoader = .green
+        case 3:
+            colorOfLoader = .orange
+        case 4:
+            colorOfLoader = .darkGray
+        default:
+            colorOfLoader = .red
+        }
+        colorsSegmentControl.selectedSegmentTintColor = colorOfLoader
     }
 
     @IBAction func buttonShowAnimation() {
-        startAnimating(type: .ballBeat)
+        
+        textFromTextView = textView.text
+        
+        startAnimating( message: textFromTextView, type: caseOfLoader, color: colorOfLoader, textColor: colorOfLoader) /*,padding: <#T##CGFloat?#>, displayTimeThreshold: <#T##Int?#>, minimumDisplayTime: <#T##Int?#>, backgroundColor: <#T##UIColor?#>, , fadeInAnimation: <#T##FadeInAnimation?##FadeInAnimation?##(UIView) -> Void#>)*/
+        
+        
+        
         
         DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3) { [weak self] in
             self?.stopAnimating()
@@ -46,6 +81,23 @@ extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        myLabel.text = typesOfLoader[row]
+        
+        switch typesOfLoader[row]{
+        case "ballPulse":
+            caseOfLoader = .ballPulse
+        case "ballSpinFadeLoader":
+            caseOfLoader = .ballSpinFadeLoader
+        case "pacman":
+            caseOfLoader = .pacman
+        case "circleStrokeSpin":
+            caseOfLoader = .circleStrokeSpin
+        case "ballTrianglePath":
+            caseOfLoader = .ballTrianglePath
+        case "squareSpin":
+            caseOfLoader = .squareSpin
+        default:
+            caseOfLoader = .ballDoubleBounce
+        }
+        
     }
 }
